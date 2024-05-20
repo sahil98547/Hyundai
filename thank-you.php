7f9d6fc855f2535c79f636e092f33bb712e0d4c4
sahil110059k@gmail.com <sa98547@gmail.com>
2024-05-20 11:48:47 +0530

    changed header and footer logo

---
+++ thank-you.php
@@ -0,0 +1,307 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+    <title>Hyundai</title>
+    <link rel="shortcut icon" type="image/png" href="favicon.ico"/>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;1,400;1,500&display=swap" rel="stylesheet">
+    <link defer="" rel="preload" as="style" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900&display=swap" onload="this.onload=null;this.rel='stylesheet'">
+    <!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" />
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/custom.css">
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W78KHGB');</script>
+<!-- End Google Tag Manager -->
+<style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  background-color: #f1f1f1;
+}
+
+#regForm {
+  background-color: #ffffff;
+  margin: 0px auto;
+  font-family: Raleway;
+  padding: 15px;
+  width: 100%;
+  min-width: 300px;
+  margin-bottom:30px;
+}
+@media (min-width: 768px) {
+.find-the-hyundai .find-the-right-car{
+    height: 70%;
+}
+.find-the-hyundai{
+        background-size: cover;
+    background-position: 0vh -6vh;
+    height: 75vh;
+}
+  #regForm {
+    background-color: #ffffff;
+    margin: 100px auto;
+    font-family: Raleway;
+    padding: 40px;
+    width: 80%;
+    min-width: 300px;
+  }
+}
+h1 {
+  text-align: center;  
+}
+
+input {
+  padding: 10px;
+  width: 100%;
+  font-size: 17px;
+  font-family: Raleway;
+  border: 1px solid #aaaaaa;
+  margin-top: 10px;
+}
+select,textarea{
+    margin-top: 10px;
+}
+input[type=radio]{
+  width: auto;
+}
+/* Mark input boxes that gets an error on validation: */
+input.invalid {
+  background-color: #ffdddd;
+}
+
+/* Hide all steps by default: */
+.tab {
+  display: none;
+}
+h1,h5{
+    margin-bottom:15px;
+}
+button {
+  background-color: #04AA6D;
+  color: #ffffff;
+  border: none;
+  padding: 10px 20px;
+  font-size: 17px;
+  font-family: Raleway;
+  cursor: pointer;
+}
+
+button:hover {
+  opacity: 0.8;
+}
+input[type="number"]{
+  border-radius: 0;
+}
+#prevBtn {
+  background-color: #bbbbbb;
+}
+
+/* Make circles that indicate the steps of the form: */
+.step {
+  height: 15px;
+  width: 15px;
+  margin: 0 2px;
+  background-color: #bbbbbb;
+  border: none;  
+  border-radius: 50%;
+  display: inline-block;
+  opacity: 0.5;
+}
+
+.step.active {
+  opacity: 1;
+}
+
+/* Mark the steps that are finished and valid: */
+.step.finish {
+  background-color: #04AA6D;
+}
+textarea {
+    overflow: auto;
+    resize: vertical;
+    width: 100%;
+}
+section.find-the-hyundai:after{
+    background:none!important;
+}
+@media (max-width: 767px){
+    .find-the-hyundai {
+    margin-top:0;
+    background-image:url('images/Square@4x-100.jpg')!important;
+}
+.find-the-right-car{
+    display:block!important;
+    font-size:18px!important;
+}
+}
+
+</style>
+</head>
+<body class="weddings-photographers-page">
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W78KHGB"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+    <div id="mobile" class="demo1">
+        <!--<div id="burgerBtn"></div>
+        <ul id="nav">
+          <li>
+            <ul>
+                <li>
+                  <input type="checkbox" checked>
+                  
+                  <h6>Nav1</h6>
+                  <!-- <div class="mobile-navigation">
+                    <a href="#"><span class="nav-img-icon"><img src="images/category-images/makeup-artists.svg" width="30" height="22.5px" alt="makeup-artists"></span>Makeup Artists</a>
+                    <a href="#"><span class="nav-img-icon"><img src="images/category-images/photographers.svg" width="30" height="22.5px" alt="photographers"></span>Photographers</a>
+                    <a href="#"><span class="nav-img-icon"><img src="images/category-images/wedding-venue.svg" width="30" height="22.5px" alt="wedding-venue"></span>Wedding Venue</a>
+                    <a href="#"><span class="nav-img-icon"><img src="images/category-images/wedding-decorators.svg" width="30" height="22.5px" alt="wedding-decorators"></span>Wedding Decorators</a>
+                    <a href="#"><span class="nav-img-icon"><img src="images/category-images/mehndi-artists.svg" width="30" height="22.5px" alt="mehndi-artists"></span>Mehndi Artists</a>
+                    <a href="#"><span class="nav-img-icon"><img src="images/category-images/wedding-planner.svg" width="30" height="22.5px" alt="wedding-planner"></span>Wedding Planner</a>
+                    <a href="#"><span class="nav-img-icon"><img src="images/category-images/wedding-catering.svg" width="30" height="22.5px" alt="wedding-catering"></span>Wedding Catering</a>
+                    <a href="#"><span class="nav-img-icon"><img src="images/category-images/bridal-wear.svg" width="30" height="22.5px" alt="bridal-wear"></span>Bridal Wear</a>
+                    <a href="#"><span class="nav-img-icon"><img src="images/category-images/wedding-gifts.svg" width="30" height="22.5px" alt="wedding-gifts"></span>Wedding Gifts</a>
+                    <a href="#"><span class="nav-img-icon"><img src="images/category-images/destination-wedding.svg" width="30" height="22.5px" alt="destination-wedding"></span>Destination Wedding</a>
+                    <a href="#"><span class="nav-img-icon"><img src="images/category-images/673025-2020-09-01-05-01-37.svg" width="30" height="22.5px" alt="sangeet-choreographers"></span>Wedding choreographers</a>
+                    <a href="#"><span class="nav-img-icon"><img src="images/category-images/wedding-dj.svg" width="30" height="22.5px" alt="wedding-dj"></span>Wedding DJ</a>
+                    <a href="#"><span class="nav-img-icon"><img src="images/category-images/groom-wear.svg" width="30" height="22.5px" alt="groom-wear"></span>Groom Wear</a>
+                    <a href="#"><span class="nav-img-icon"><img src="images/category-images/208072-2020-09-01-05-01-42.svg" width="30" height="22.5px" alt="entertainment"></span>Wedding Entertainment</a>
+                    <a href="#"><span class="nav-img-icon"><img src="images/category-images/trousseau-packers.svg" width="30" height="22.5px" alt="trousseau-packers"></span>Trousseau Packers</a>
+                    <a href="#"><span class="nav-img-icon"><img src="images/category-images/wedding-accessories.svg" width="30" height="22.5px" alt="wedding-accessories"></span>Wedding Accessories</a>
+                    <a href="#"><span class="nav-img-icon"><img src="images/category-images/wedding-cakes.svg" width="30" height="22.5px" alt="wedding-cakes"></span>Wedding Cakes</a>
+                    <a href="#"><span class="nav-img-icon"><img src="images/category-images/wedding-card.svg" width="30" height="22.5px" alt="wedding-card"></span>Wedding Card</a>
+                    <a href="#"><span class="nav-img-icon"><img src="images/category-images/wedding-jewellery.svg" width="30" height="22.5px" alt="wedding-jewellery"></span>Wedding Jewellery</a>
+                    <a href="#"><span class="nav-img-icon"><img src="images/category-images/wedding-videography.svg" width="30" height="22.5px" alt="wedding-videography"></span>Wedding Videography</a>
+                  </div>
+                </li>
+                <li>
+                  <input type="checkbox" checked>
+                  <h6><a href="#">Nav2</a></h6>
+                </li>
+                <li>
+                  <input type="checkbox" checked>
+                  <h6><a href="#">Nav13</a></h6>
+                </li>
+              </ul>
+          </li>
+        </ul>-->
+        <div id="mobileBodyContent">
+          <div id="header">
+              <span class="logo">
+              <img src="https://hyundailucknow.com/images/beeaar-hyundai.webp" alt="Hyundai">
+              </span>
+              <span class="show-models">
+               <a href="tel:8000786424" onclick="gtag_report_conversion(url)"><i class="fa-solid fa-phone"></i> 8000786424</a>
+              </span>
+          </div>
+          <section class="find-the-hyundai" style="background-image:url('images/Rec@4x-100.jpg')">
+              <!-- <img src="images/venu-car.jpg" alt="" /> -->
+             <div class="find-the-right-car" style="display:none"><i>CELEBRATING 25 GLORIOUS YEAR!</i></div>
+          </section>
+
+          
+
+          <div class="get-a-quotes text-center ">
+          
+              <div class="container">
+                  <h2>Thank you for being associated with Beeaar Hyundai. We will get back to you soon.</h2>
+                    
+              </div>
+          </div>
+
+          
+
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                    <img src="https://hyundailucknow.com/images/beeaar-hyundai.webp" class="footer-logo">
+                        <p>Beeaar Hyundai means fixed right, priced right and right on time! Here you experience a unique feature of having the honor of servicing highest number of car in the city</p>
+                    </div>
+                   <!-- <div class="col-md-2">
+                        <div class="footer-heading">Quick Links</div>
+                        <ul>
+                            <li><a href="#">ABOUT US</a></li>
+                            <li><a href="#">CARS</a></li>
+                            <li><a href="#">SERVICE</a></li>
+                            <li><a href="#">USED CARS</a></li>
+                            <li><a href="#">FINANCE</a></li>
+                            <li><a href="#">OFFERS</a></li>
+                            <li><a href="#">ACCESSORIES</a></li>
+                            <li><a href="#">CONTACT</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3 mobile-block">
+                        <div class="footer-heading">Schedule an Expert Callback</div>
+                        <div class="footer-form">
+                            <input type="text" placeholder="Your Name *" />
+                            <input type="number" placeholder="Contact No *" />
+                            <button class="btn submit">Submit</button>
+                        </div>
+                        <div class="follow-us">
+                            <div class="footer-heading">Follow Us on</div>
+                            <ul class="follow-us-blog">
+                                <li><a href="#" class="facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                                <li><a href="#" class="instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                              </ul>
+                        </div>
+                    </div> -->
+                    <div class="col-md-3">
+                      <div class="footer-subheading">Locations</div>
+                        <ul class="locations">
+                            <li><a href="https://goo.gl/maps/o4ZHi8jdX6Jiw6V78" target="_blank"><i class="fa-solid fa-location-dot"></i> Hajratganj</a></li>
+                            <li><a href="https://g.page/BeeaarHyundai?share" target="_blank"><i class="fa-solid fa-location-dot"></i> Mahanagar</a></li>
+                            <li><a href="https://goo.gl/maps/5MLpn7iQgb1QXdKs5" target="_blank"><i class="fa-solid fa-location-dot"></i> Balrampur</a></li>
+                            <li><a href="https://g.page/beeaar-hyundai---sitapur?share" target="_blank"><i class="fa-solid fa-location-dot"></i> Sitapur</a></li>
+                            <li><a href="https://goo.gl/maps/2prGHLDuBNF2ymya9" target="_blank"><i class="fa-solid fa-location-dot"></i> Basti</a></li>
+                        </ul>
+                   </div>
+                    <div class="col-md-3">
+                        <div class="footer-subheading">Social Links</div>
+                        <ul class="social-links">
+                        <li><a href="https://www.facebook.com/HyundaiLucknow/" target="_blank"><i class="fa-brands fa-facebook"></i></a></li>
+                        <li><a href="https://www.instagram.com/beeaarhyundai/" target="_blank"><i class="fa-brands fa-square-instagram"></i></a></li>
+                        <li><a href="https://in.linkedin.com/company/beeaar-hyundai-lucknow" target="_blank"><i class="fa-brands fa-linkedin"></i></a></li>
+                        <li><a href="https://www.youtube.com/hyundailucknow" target="_blank"><i class="fa-brands fa-youtube"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="sub-footer">
+                <div class="container">
+                    Design and Develop by <a href="https://step1inc.com/" target="_blank">Step1inc</a>
+                </div>
+            </div>
+        </footer>
+
+        </div>
+        <div class="price-block-fixed"> 
+            <div class="price-fixed-scroll">
+                <a href="#lead-form-login" class="get-in-touch">Check Latest Offers</a>
+                <a href="//api.whatsapp.com/send?phone=917521001038&text=Provide your *Name:* and *Location:* to get started" class="get-whats-app"  onclick="gtag_report_conversion1(url)"><i class="fa-brands fa-whatsapp"></i></a>
+                <a href="tel:+918000786424" class="get-mobile" onclick="gtag_report_conversion(url)"><i class="fa-solid fa-phone"></i></a>
+            </div>
+        </div>
+    </div>
+    <?php
+    include 'leadform.php';
+    ?>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript" src="js/owl.carousel.min.js"></script>
+    <script type="text/javascript" src="js/custom.js"></script>
+</body>
+</html>
+
